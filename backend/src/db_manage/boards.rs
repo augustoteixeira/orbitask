255a1b255e79fd65ec8004cffa8137a430b791b5
Augusto Teixeira <augusto@impa.br>
2025-05-23 14:06:02 -0300

    chores: split db_manage boards logic

---
+++ backend/src/db_manage/boards.rs
@@ -0,0 +1,61 @@
+use crate::sqlx::FromRow;
+use rocket_db_pools::sqlx::{self};
+use rocket_db_pools::Connection;
+
+use super::Db;
+
+#[derive(Debug, FromRow)]
+pub struct Board {
+    pub id: i64,
+    pub name: String,
+    pub is_template: bool,
+}
+
+pub async fn get_board(
+    mut db: Connection<Db>,
+    board_id: i64,
+) -> Result<Option<Board>, sqlx::Error> {
+    let board = sqlx::query_as::<_, Board>(
+        "SELECT id, name, is_template FROM boards WHERE id = ?",
+    )
+    .bind(board_id)
+    .fetch_optional(&mut **db)
+    .await?;
+
+    Ok(board)
+}
+
+pub async fn get_all_boards(
+    mut db: Connection<Db>,
+    include_templates: bool,
+) -> Result<Vec<Board>, sqlx::Error> {
+    let rows: Vec<Board> = sqlx::query_as::<_, Board>(
+        format!(
+            r#"
+      SELECT id, name, is_template
+      FROM boards
+      {:}
+      ORDER BY name
+        "#,
+            if include_templates {
+                "".to_string()
+            } else {
+                "WHERE is_template = 0".to_string()
+            }
+        )
+        .as_str(),
+    )
+    .fetch_all(&mut **db)
+    .await?;
+
+    let boards = rows
+        .into_iter()
+        .map(|row| Board {
+            id: row.id,
+            name: row.name,
+            is_template: row.is_template,
+        })
+        .collect();
+
+    Ok(boards)
+}
--- backend/src/db_manage/mod.rs
+++ backend/src/db_manage/mod.rs
@@ -1,80 +1,16 @@
 use std::fs;
 
-use crate::sqlx::FromRow;
 use rocket_db_pools::sqlx::{self};
-use rocket_db_pools::{Connection, Database};
+use rocket_db_pools::Database;
 
 pub mod login;
 pub use login::{get_password, set_password};
+pub mod boards;
+pub use boards::{get_all_boards, get_board, Board};
 
 #[derive(Database)]
 #[database("db")]
 pub struct Db(sqlx::SqlitePool);
-
-#[derive(Debug, FromRow)]
-pub struct Board {
-    pub id: i64,
-    pub name: String,
-    pub is_template: bool,
-}
-
-pub async fn get_board(
-    mut db: Connection<Db>,
-    board_id: i64,
-) -> Result<Option<Board>, sqlx::Error> {
-    let board = sqlx::query_as::<_, Board>(
-        "SELECT id, name, is_template FROM boards WHERE id = ?",
-    )
-    .bind(board_id)
-    .fetch_optional(&mut **db)
-    .await?;
-
-    Ok(board)
-}
-
-pub async fn get_all_boards(
-    mut db: Connection<Db>,
-    include_templates: bool,
-) -> Result<Vec<Board>, sqlx::Error> {
-    let rows: Vec<Board> = sqlx::query_as::<_, Board>(
-        format!(
-            r#"
-      SELECT id, name, is_template
-      FROM boards
-      {:}
-      ORDER BY name
-        "#,
-            if include_templates {
-                "".to_string()
-            } else {
-                "WHERE is_template = 0".to_string()
-            }
-        )
-        .as_str(),
-    )
-    .fetch_all(&mut **db)
-    .await?;
-
-    let boards = rows
-        .into_iter()
-        .map(|row| Board {
-            id: row.id,
-            name: row.name,
-            is_template: row.is_template,
-        })
-        .collect();
-
-    Ok(boards)
-}
-
-// pub async fn get_board(mut db: Connection<Db>, id: i64) -> Option<String> {
-//     sqlx::query("SELECT content FROM boards WHERE id = ?")
-//         .bind(id)
-//         .fetch_one(&mut **db)
-//         .await
-//         .and_then(|r| Ok(r.try_get(0)?))
-//         .ok()
-// }
 
 pub async fn migrate(conn: &Db) -> Result<(), sqlx::Error> {
     // Create meta table if it doesn't exist
